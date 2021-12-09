70cb7030a29880b1efb52bf657f56ae51d2536e2
Paulo <paulofreire@frwk.com.br>
2021-12-09 10:40:12 -0300

    add delete and edit categories

--- CrudApi.WebUi/Controllers/CategoriesController.cs
+++ CrudApi.WebUi/Controllers/CategoriesController.cs
@@ -65,5 +65,38 @@ namespace CrudApi.WebUi.Controllers
             }
             return View(categoryDto);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete (int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+
+        }
+
     }
 }
